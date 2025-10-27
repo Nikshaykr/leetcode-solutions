d3499f0af7b3d381eec7c35f5f25fa10f1a7fdb4
nikshay <nikshaykr56@gmail.com>
2025-10-27 21:38:10 +0530

    Added a better solution to PalindromeNumber

--- src/Easy/PalindromeNumber.java
+++ src/Easy/PalindromeNumber.java
@@ -6,4 +6,18 @@ public class PalindromeNumber {
         String rev = new StringBuilder(str).reverse().toString();
         return str.equals(rev);
     }
+
+    public boolean isPalindromeBetter(int x) {
+        var characters = String.valueOf(x).toCharArray();
+        var left = 0;
+        var right = characters.length - 1;
+        while (left < right) {
+            if (characters[left] != characters[right]) return false;
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+    // Time complexity: O(log n) & Space complexity: O(log n) because of string and char array storage
 }
