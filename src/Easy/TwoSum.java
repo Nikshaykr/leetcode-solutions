a4ad1cc7ab4eb913de7de7e1db4dc4132bfb07b8
nikshay <nikshaykr56@gmail.com>
2025-10-27 20:16:26 +0530

    Added a better solution to TwoSum

--- src/Easy/TwoSum.java
+++ src/Easy/TwoSum.java
@@ -1,5 +1,8 @@
 package Easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Brute force solution for Two Sum problem
  * Time: O(n²), Space: O(1)
@@ -19,5 +22,24 @@ public class TwoSum {
             }
         }
         return result;
+    }
+    
+    /**
+     * Optimized HashMap solution for Two Sum problem
+     * Time: O(n), Space: O(n)
+     */
+    public int[] twoSumBetter(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            // Check if complement exists in map
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
+            }
+            // Store current number and its index
+            map.put(nums[i], i);
+        }
+        return new int[2];
     }
 }
