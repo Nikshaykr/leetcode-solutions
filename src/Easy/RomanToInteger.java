0a6ea211981bdd692713d3b01f2e3271041c47f5
nikshay <nikshaykr56@gmail.com>
2025-10-28 22:52:47 +0530

    Solved 13. Roman To Integer

---
+++ src/Easy/RomanToInteger.java
@@ -0,0 +1,20 @@
+package Easy;
+
+import java.util.Map;
+
+public class RomanToInteger {
+
+    public int romanToInt (String s){
+        Map<Character, Integer> map = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50,
+                                             'C', 100, 'D', 500, 'M', 1000);
+        var result = 0;
+        for (int i = s.length() - 1; i >=0; i--){
+            if (i + 1 < s.length() && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+}
