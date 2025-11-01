c72b0a898128a61c889d4ca925c8e9523986b759
nikshay <nikshaykr56@gmail.com>
2025-11-01 15:46:50 +0530

    Add comments to Container With Most Water

--- src/Medium/ContainerWithMostWater.java
+++ src/Medium/ContainerWithMostWater.java
@@ -2,15 +2,20 @@ package Medium;
 
 public class ContainerWithMostWater {
     public int maxArea(int[] height) {
+
+        // Initialize left and right pointer and assume maxArea to be 0
         int left = 0;
         int right = height.length - 1;
         int maxArea = 0;
 
+        // While left pointer does not cross right pointer calculate the area of the current rectangle formed by
+        // the left and right pointer by formula area of rectangle = length * breadth, and update the maxArea
         while (left < right) {
             int currentArea = Math.min(height[left], height[right]) * (right - left);
             maxArea = Math.max(maxArea, currentArea);
 
-            if (height[left] < height[right]) {
+            // If the left height is lesser or equal to the right height move the left pointer inward to find a higher height
+            if (height[left] <= height[right]) {
                 left++;
             } else {
                 right--;
