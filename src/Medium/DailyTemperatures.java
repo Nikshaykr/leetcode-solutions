39defbd25dd8e8bca917c69d89c4dda7d06bad44
nikshay <nikshaykr56@gmail.com>
2025-11-11 16:46:05 +0530

    Solved 739. Daily Temperatures

---
+++ src/Medium/DailyTemperatures.java
@@ -0,0 +1,33 @@
+package Medium;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int n = temperatures.length;
+        int[] result = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                result[i] = stack.peek() - i;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        DailyTemperatures solutions = new DailyTemperatures();
+
+        // Test case 1
+        int[] temperatures1 = {73, 74, 75, 71, 69, 72, 76, 73};
+        int[] result1 = solutions.dailyTemperatures(temperatures1);
+        System.out.println(Arrays.toString(result1));
+
+    }
+}
